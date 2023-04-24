1a363496d826aeab86d887d62703d0463b127227
Joel Detweiler <joel.detweiler@gmail.com>
2023-04-24 14:20:04 -0700

    Set up migrations

--- week3/migrate-mongo-config.js
+++ week3/migrate-mongo-config.js
@@ -8,6 +8,8 @@ const config = {
     options: {
       useNewUrlParser: true, // removes a deprecation warning when connecting
       useUnifiedTopology: true, // removes a deprecating warning when connecting
+      //   connectTimeoutMS: 3600000, // increase connection timeout to 1 hour
+      //   socketTimeoutMS: 3600000, // increase socket timeout to 1 hour
     }
   },
 
@@ -18,8 +20,14 @@ const config = {
   changelogCollectionName: "changelog",
 
   // The file extension to create migrations and search for in migration dir 
-  migrationFileExtension: ".js"
+  migrationFileExtension: ".js",
+
+  // Enable the algorithm to create a checksum of the file contents and use that in the comparison to determine
+  // if the file should be run.  Requires that scripts are coded to be run multiple times.
+  useFileHash: false,
+
+  // Don't change this, unless you know what you're doing
+  moduleSystem: 'commonjs',
 };
 
-// Return the config as a promise
 module.exports = config;
